9def4fc4521ef67698c7d7d393a8c8256039b655
Clayton Sanford <clayton_sanford@brown.edu>
2019-11-10 18:44:33 -0500

    testing convergence for many iterations with simple functions

--- simple_functions_test.py
+++ simple_functions_test.py
@@ -3,8 +3,13 @@ from simple_testing.cosine_function import Cosine
 from simple_testing.octopus_function import Octopus
 from simple_testing.quadratic_function import Quadratic
 from simple_testing.simple_testing import run
+from simple_testing.simple_testing import run_trials
 
-gd_function_optimizer = GradientDescent(False, learning_rate=0.1)
-run(Quadratic(dim=10), 100, gd_function_optimizer, 0.1, False)
-run(Cosine(dim=10), 100, gd_function_optimizer, 0.1, False)
-run(Octopus(), 100, gd_function_optimizer, 0.1, True)
+
+# vanilla_gd_function_optimizer = GradientDescent(False, learning_rate=0.1, noise_r=0, noise_T=0, noise_eps=0,
+#                  momentum=0, NCE=False, NCE_s=0, NCE_gamma=0)
+default_agd_function_optimizer = GradientDescent(False)
+# run(Quadratic(dim=10), 100, gd_function_optimizer, 0.1, False)
+# run(Cosine(dim=10), 100, gd_function_optimizer, 0.1, False)
+# run(Octopus(), 100, gd_function_optimizer, 0.1, True)
+run_trials(Octopus(), default_agd_function_optimizer)
--- simple_testing/simple_testing.py
+++ simple_testing/simple_testing.py
@@ -2,21 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def run(f, epochs, optimizer, epsilon, is_verbose):
-    print(f.as_string() + "\n")
+# Returns a tuple: (# steps to first-order stationary point, # steps to second-order stationary point)
+def run(f, optimizer, epsilon=0.1, epochs=100, verbosity=1, create_plot=True):
+    if verbosity >= 2:
+        print(f.as_string() + "\n")
 
     steps_to_fosp = None
     steps_to_sosp = None
 
     x = f.random_init()
     f_x_vector = [f.eval(x)]
-    if is_verbose:
+    if verbosity >= 2:
         print_epoch(0, f, x, epsilon)
 
     for e in range(1, epochs + 1):
-        x = optimizer.step_not_ml(f, x, is_verbose)
+        x = optimizer.step_not_ml(f, x, is_verbose=(verbosity >= 2))
         f_x_vector.append(f.eval(x))
-        if is_verbose:
+        if verbosity >= 2:
             print_epoch(e, f, x, epsilon)
 
         if steps_to_fosp == None and is_first_order_stationary_point(f, x, epsilon):
@@ -24,18 +26,49 @@ def run(f, epochs, optimizer, epsilon, is_verbose):
         if steps_to_sosp == None and is_second_order_stationary_point(f, x, epsilon):
             steps_to_sosp = e
 
-    if steps_to_fosp == None:
-        print("Did not converge to a " + str(epsilon) + "-first-order stationary-point.")
-    else:
-        print(
-            "Converged to a " + str(epsilon) + "-first-order stationary-point after " + str(steps_to_fosp) + " steps.")
-    if steps_to_sosp == None:
-        print("Did not converge to a " + str(epsilon) + "-second-order stationary-point.")
-    else:
-        print(
-            "Converged to a " + str(epsilon) + "-second-order stationary-point after " + str(steps_to_sosp) + " steps.")
+    if verbosity >= 1:
+        if steps_to_fosp == None:
+            print("Did not converge to a " + str(epsilon) + "-first-order stationary-point.")
+        else:
+            print(
+                "Converged to a " + str(epsilon) + "-first-order stationary-point after " + str(steps_to_fosp) + " steps.")
+        if steps_to_sosp == None:
+            print("Did not converge to a " + str(epsilon) + "-second-order stationary-point.")
+        else:
+            print(
+                "Converged to a " + str(epsilon) + "-second-order stationary-point after " + str(steps_to_sosp) + " steps.")
 
-    plot_results(epochs, f_x_vector)
+    if create_plot:
+        plot_results(epochs, f_x_vector)
+    return (steps_to_fosp, steps_to_sosp)
+
+def run_trials(f, optimizer, trials=1000, epsilon=0.1, epochs=200, verbosity=0):
+    if verbosity >= 0:
+        print(f.as_string() + "\n")
+
+    list_steps_to_fosp = []
+    list_steps_to_sosp = []
+    count_no_terminate = 0
+    for i in range(trials):
+        (fosp, sosp) = run(f, optimizer, epsilon=epsilon, epochs=epochs, verbosity=verbosity, create_plot=False)
+        if verbosity >= 1:
+            print("\nTrial " + str(i))
+        if (sosp == None):
+            count_no_terminate += 1
+        else:
+            list_steps_to_fosp.append(fosp)
+            list_steps_to_sosp.append(sosp)
+
+    if verbosity >= 0:
+        print("Trials without convergence in " + str(epochs) + " epochs: " + str(count_no_terminate))
+    bins = np.linspace(0, epochs, epochs)
+    plt.hist([list_steps_to_fosp, list_steps_to_sosp], bins, label=['Steps to FOSP', 'Steps to SOSP'])
+    plt.legend(loc='upper right')
+    plt.xlabel('epochs to convergence')
+    plt.ylabel('number of trials')
+    plt.show()
+
+
 
 
 def plot_results(epochs, f_x_vector):
