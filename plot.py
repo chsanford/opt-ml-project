994a8cb6eba259c5cbae79e08501993dc102000d
GingerBell <linda6582@163.com>
2019-11-24 12:51:51 -0500

    data&plot for small NCE_s

--- plot.py
+++ plot.py
@@ -142,34 +142,33 @@ fig.savefig("oct_loss.png", dpi=600)
 # ================
 # ML FUNCTIONS
 # ================
-'''
 # FNN optimizers
 fnn_test = MnistTest(ff=True)
 fnn_optimizer = dict()
 fnn_optimizer['gd'] = GradientDescent(fnn_test.model.parameters(), is_ml=True, lr=0.25, momentum=0, noise_r=0, NCE=False, is_verbose=False)
-fnn_optimizer['agd'] = GradientDescent(fnn_test.model.parameters(), is_ml=True, lr=0.18, momentum=0.95, noise_r=0, NCE=False, is_verbose=False)
-fnn_optimizer['agd_noise'] = GradientDescent(fnn_test.model.parameters(), is_ml=True, lr=0.18, momentum=0.95, noise_r=0.1, noise_T=50, noise_eps=0.1, NCE=False, is_verbose=False)
-fnn_optimizer['agd_NCE'] = GradientDescent(fnn_test.model.parameters(), is_ml=True, lr=0.18, momentum=0.95, noise_r=0, noise_T=50, noise_eps=0.1, NCE=True, NCE_s=7, is_verbose=False)
-fnn_optimizer['all'] = GradientDescent(fnn_test.model.parameters(), is_ml=True, lr=0.18, momentum=0.95, noise_r=0.1, noise_T=50, noise_eps=0.1, NCE=True, NCE_s=4, is_verbose=False)
+#fnn_optimizer['agd'] = GradientDescent(fnn_test.model.parameters(), is_ml=True, lr=0.18, momentum=0.95, noise_r=0, NCE=False, is_verbose=False)
+#fnn_optimizer['agd_noise'] = GradientDescent(fnn_test.model.parameters(), is_ml=True, lr=0.18, momentum=0.95, noise_r=0.1, noise_T=50, noise_eps=0.1, NCE=False, is_verbose=False)
+fnn_optimizer['agd_NCE'] = GradientDescent(fnn_test.model.parameters(), is_ml=True, lr=0.18, momentum=0.95, noise_r=0, noise_T=50, noise_eps=0.1, NCE=True, NCE_s=0.1, is_verbose=False)
+fnn_optimizer['all'] = GradientDescent(fnn_test.model.parameters(), is_ml=True, lr=0.18, momentum=0.95, noise_r=0.1, noise_T=50, noise_eps=0.1, NCE=True, NCE_s=0.1, is_verbose=False)
 
 # CNN optimizers
 cnn_test = MnistTest(ff=False)
 cnn_optimizer = dict()
 cnn_optimizer['gd'] = GradientDescent(cnn_test.model.parameters(), is_ml=True, lr=0.26, momentum=0, noise_r=0, NCE=False, is_verbose=False)
-cnn_optimizer['agd'] = GradientDescent(cnn_test.model.parameters(), is_ml=True, lr=0.05, momentum=0.9, noise_r=0, NCE=False, is_verbose=False)
-cnn_optimizer['agd_noise'] = GradientDescent(cnn_test.model.parameters(), is_ml=True, lr=0.05, momentum=0.9, noise_r=1, noise_T=50, NCE=False, is_verbose=False)
-cnn_optimizer['agd_NCE'] = GradientDescent(cnn_test.model.parameters(), is_ml=True, lr=0.05, momentum=0.9, noise_r=0, NCE=True, NCE_s=6, is_verbose=False)
-cnn_optimizer['all'] = GradientDescent(cnn_test.model.parameters(), is_ml=True, lr=0.05, momentum=0.9, noise_r=1, noise_T=50, NCE=True, NCE_s=6, is_verbose=False)
-'''
+#cnn_optimizer['agd'] = GradientDescent(cnn_test.model.parameters(), is_ml=True, lr=0.05, momentum=0.9, noise_r=0, NCE=False, is_verbose=False)
+#cnn_optimizer['agd_noise'] = GradientDescent(cnn_test.model.parameters(), is_ml=True, lr=0.05, momentum=0.9, noise_r=1, noise_T=50, NCE=False, is_verbose=False)
+cnn_optimizer['agd_NCE'] = GradientDescent(cnn_test.model.parameters(), is_ml=True, lr=0.05, momentum=0.9, noise_r=0, NCE=True, NCE_s=0.1, is_verbose=False)
+cnn_optimizer['all'] = GradientDescent(cnn_test.model.parameters(), is_ml=True, lr=0.05, momentum=0.9, noise_r=1, noise_T=50, NCE=True, NCE_s=0.1, is_verbose=False)
 
+'''
 # Matrix Factorization optimizers
-mf_test = MatrixFactorizationTest(load_model=False)
+mf_test = MatrixFactorizationTest(load_model=True)
 mf_optimizer = dict()
-#mf_optimizer['gd'] = GradientDescent(mf_test.model.parameters(), is_ml=True, lr=36, momentum=0, noise_r=0, NCE=False, is_verbose=False)
-#mf_optimizer['gd_noise'] = GradientDescent(mf_test.model.parameters(), is_ml=True, lr=36, momentum=0, noise_r=1, noise_T=1, noise_eps=0.1, NCE=False, is_verbose=False)
+mf_optimizer['gd'] = GradientDescent(mf_test.model.parameters(), is_ml=True, lr=36, momentum=0, noise_r=0, NCE=False, is_verbose=False)
+mf_optimizer['gd_noise'] = GradientDescent(mf_test.model.parameters(), is_ml=True, lr=36, momentum=0, noise_r=1, noise_T=1, noise_eps=0.1, NCE=False, is_verbose=False)
 mf_optimizer['agd'] = GradientDescent(mf_test.model.parameters(), is_ml=True, lr=22, momentum=0.95, noise_r=0, NCE=False, is_verbose=False)
 mf_optimizer['all'] = GradientDescent(mf_test.model.parameters(), is_ml=True, lr=22, momentum=0.95, noise_r=1, noise_T=1, noise_eps=0.1, NCE=False, is_verbose=False)
-
+'''
 
 # run experiments
 trials=dict()
@@ -180,9 +179,8 @@ trials['mf'] = 10
 epochs['fnn'] = 200
 epochs['cnn'] = 200
 epochs['mf'] = 200
-#all_filenames={'fnn':dict(), 'cnn':dict()}
-all_filenames={'fnn':dict(), 'cnn':dict(), 'mf':dict()}
-'''
+all_filenames={'fnn':dict(), 'cnn':dict()}
+#all_filenames={'fnn':dict(), 'cnn':dict(), 'mf':dict()}
 for name, optimizer in fnn_optimizer.items():
     print(f'Train FNN using optimizer {name}')
     all_filenames['fnn'][name] = fnn_test.run(epochs['fnn'], optimizer, log=True, trials=trials['fnn'], tag=name)
@@ -204,6 +202,7 @@ all_filenames['cnn']['agd'] = './log/CNN-agd-11-24T07:38:06'
 all_filenames['cnn']['agd_noise'] = './log/CNN-agd_noise-11-24T08:15:14'
 all_filenames['cnn']['agd_NCE'] = './log/CNN-agd_NCE-11-24T08:55:03'
 all_filenames['cnn']['all'] = './log/CNN-all-11-24T09:34:29'
+'''
 
 # save average data
 for fname, filenames in all_filenames.items():
@@ -226,20 +225,39 @@ for fname, filenames in all_filenames.items():
             print_(f'mean_test_loss = {mean_test}')
             print_(f'std_test_loss = {std_test}')
 
+
 # make loss graph
+# all
 for fname, filenames in all_filenames.items():
-    logs = load_logs([filenames['gd'], filenames['agd_noise']])
+    logs = load_logs([filenames['gd'], filenames['all']])
     fig, ax = plt.subplots()
     ax.margins(0)
     ax.set_xlabel('epochs')
     ax.set_ylabel('losses')
     ax.plot(np.mean(logs[0]['train_losses'], axis=0), label='GD (Train)', color='#1f77b4')
-    ax.plot(np.mean(logs[1]['train_losses'], axis=0), label='AGD with noise (Train)', color='#ff7f0e')
+    ax.plot(np.mean(logs[1]['train_losses'], axis=0), label='AGD with noise and NCE (Train)', color='#ff7f0e')
     if not fname == 'cnn':
         ax.plot(np.mean(logs[0]['test_losses'], axis=0), '--', label='GD (Test)', color='#1f77b4')
-        ax.plot(np.mean(logs[1]['test_losses'], axis=0), '--', label='AGD with noise (Test)', color='#ff7f0e')
+        ax.plot(np.mean(logs[1]['test_losses'], axis=0), '--', label='AGD with noise and NCE (Test)', color='#ff7f0e')
     else:
         ax.set_ylim(top=4) 
     ax.legend()
-    fig.savefig(fname+"_agd_noise.png", dpi=600)
+    fig.savefig(fname+"_all.png", dpi=600)
+
+# agd_NCE
+for fname, filenames in all_filenames.items():
+    logs = load_logs([filenames['gd'], filenames['agd_NCE']])
+    fig, ax = plt.subplots()
+    ax.margins(0)
+    ax.set_xlabel('epochs')
+    ax.set_ylabel('losses')
+    ax.plot(np.mean(logs[0]['train_losses'], axis=0), label='GD (Train)', color='#1f77b4')
+    ax.plot(np.mean(logs[1]['train_losses'], axis=0), label='AGD with NCE (Train)', color='#ff7f0e')
+    if not fname == 'cnn':
+        ax.plot(np.mean(logs[0]['test_losses'], axis=0), '--', label='GD (Test)', color='#1f77b4')
+        ax.plot(np.mean(logs[1]['test_losses'], axis=0), '--', label='AGD with NCE (Test)', color='#ff7f0e')
+    else:
+        ax.set_ylim(top=4) 
+    ax.legend()
+    fig.savefig(fname+"_NCE.png", dpi=600)
 
